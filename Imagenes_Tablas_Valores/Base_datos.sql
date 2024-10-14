861f0b4c071353abded64f899a5df95a5bae8aac
IvanCaceresS <ivan.caceres_s@mail.udp.cl>
2024-10-14 14:58:14 -0300

    actualizacion

--- Imagenes_Tablas_Valores/Base_datos.sql
+++ Imagenes_Tablas_Valores/Base_datos.sql
@@ -97,29 +97,5 @@ CREATE TABLE proyectoalgoritmos.trafico_actual (
 );
 
 
--- CREATE TABLE proyectoalgoritmos.comuna (
--- 	id SERIAL PRIMARY KEY,
--- 	name VARCHAR(255),
---     geometry GEOMETRY(polygon, 4326) 
--- );
-
--- CREATE TABLE proyectoalgoritmos.infraestructura_metadata (
--- 	id_infraestructura BIGINT PRIMARY KEY,
---     maxSpeed INT,
--- 	currentSpeed INT,
---     lit BOOLEAN DEFAULT FALSE,
---     roadClosure BOOLEAN DEFAULT FALSE,
--- 	FOREIGN KEY (id_infraestructura) 
--- 	REFERENCES proyectoalgoritmos.infraestructura(id)
--- );
-
--- CREATE TABLE proyectoalgoritmos.comuna_metadata (
--- 	id_comuna SERIAL PRIMARY KEY,
--- 	precipitacion NUMERIC(3, 9),
--- 	delincuencia NUMERIC(3, 9),
--- 	FOREIGN KEY (id_infraestructura)
--- 	REFERENCES proyectoalgoritmos.comuna(id)
--- );
-
 CREATE INDEX infraestructura_geometry_idx ON proyectoalgoritmos.infraestructura USING GIST (geometry);
 CREATE INDEX infraestructura_nodos_geometry_idx ON proyectoalgoritmos.infraestructura_nodos USING GIST (geometry);
